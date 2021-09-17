2ac597628e6ed40cac3e093fa5cdb77f815b8f04
rewine <LHongXu@outlook.com>
2021-09-18 01:50:44 +0800

    fix bug (insert)

--- PolygonScanConversio.hs
+++ PolygonScanConversio.hs
@@ -31,8 +31,8 @@ type NET = [(Int,[EdgeI])] -- 新边表
 type AET = [EdgeI] -- 活动边表
 
 main :: IO()
-main = animationOf $ drawPolys [[(0,0), (2,3)]]
-
+main = animationOf $ drawPolys [[(0,0), (2,8), (6, 2), (0,0)]]
+ 
 drawPolys:: [[PointI]] -> Double -> Picture
 drawPolys polys seconds = 
                    coordinatePlane 
@@ -41,7 +41,7 @@ drawPolys polys seconds =
                         (ploy_fill (gYMin-1) 
                                    (mkNewEdgeTables polys) -- NET
                                    []) )-- AET
-                   -- & drawDot ((length $ snd $ (mkNewEdgeTables polys)!!0),0)
+                   -- & colored red $ drawDot ((length $ snd $ (mkNewEdgeTables polys)!!2),0)
                    where 
                      lineCnt = 100 -- floor (seconds*0.5) + 1
                      
@@ -55,13 +55,14 @@ mkNewEdgeTable::[PointI] -> NET
 mkNewEdgeTable [] = zip [gYMin .. gYMax] (repeat [])
 mkNewEdgeTable [x] = zip [gYMin .. gYMax] (repeat []) -- 返回空表
 mkNewEdgeTable (a@(ax, ay) : b@(bx,by) : xs) = if ay == by then mkNewEdgeTable (b:xs) -- 过滤水平边
-                                               else insertE (min ay by) (mkEdge ax ay bx by) (mkNewEdgeTable (b:xs))  
+                                               else insertE (min ay by) (mkEdge ax ay bx by) (mkNewEdgeTable (b:xs))
+                                              
 
 -- 将边信息插入新边表
 insertE :: Int -> EdgeI -> NET -> NET
 insertE y edge (net@(posY,eds):nets) = if y == posY 
-                                      then return (posY, (edge:eds)) ++ nets
-                                      else return net ++ nets
+                                       then return (posY, (edge:eds)) ++ nets
+                                       else return net ++ insertE y edge nets
 
 -- 扫描线法主过程
 ploy_fill:: Int -> NET -> AET -> [PointI]
@@ -70,12 +71,11 @@ ploy_fill y net aet = getYvalAet y aet
                       ploy_fill (y+1)
                                 (tail net) --(filter (\(y',e) -> y'/= y+1) net) -- 删去过时的新边表
                                 ((snd $ head net)  -- 从新边表中加入新的活动边
-                                 -- ++ (filter (\aet@(EdgeI x dx yM) -> yM == y) aet)) -- 删去Ymax>y+1的活动边
                                  ++ getNext y aet)
                       where getNext:: Int -> AET -> AET
                             getNext y [] = []
                             getNext y (aet@(EdgeI x dx yM):aets)
-                              | y == yM = getNext y aets
+                              | y == yM = getNext y aets -- 删去Ymax>y+1的活动边
                               | otherwise = aet { x = x+dx } : getNext y aets
 
 -- 跟据y值和活性边表，求当前扫描线的所有交
